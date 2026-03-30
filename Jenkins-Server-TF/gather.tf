a744a97a069e734d5f74e74f509f97412134f3eb
Harshithacm <harshithacm77@gmail.com>
2026-03-30 16:48:57 +0530

    initial commit

---
+++ Jenkins-Server-TF/gather.tf
@@ -0,0 +1,9 @@
+data "aws_ami" "ami" {
+    most_recent = true
+    filter {
+        name   = "name"
+        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+    }
+    owners = ["271691289720"]
+  
+}
