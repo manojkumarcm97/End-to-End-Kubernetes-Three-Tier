93e1b47465455afe1e6f02ea8b74b7dc1a20b64d
manoj kumar <manojkumarcm97@gmail.com>
2026-03-31 16:40:48 +0530

    dsj

--- terraform/tools-install.sh
+++ terraform/tools-install.sh
@@ -16,7 +16,6 @@ sudo apt-get update -y
 sudo apt-get install jenkins -y
 
 # Installing Docker 
-#!/bin/bash
 sudo apt update
 sudo apt install docker.io -y
 sudo usermod -aG docker jenkins
@@ -28,19 +27,16 @@ sudo chmod 777 /var/run/docker.sock
 # docker run -d -p 8080:8080 -p 50000:50000 --name jenkins-container jenkins/jenkins:lts
 
 # Run Docker Container of Sonarqube
-#!/bin/bash
 docker run -d  --name sonar -p 9000:9000 sonarqube:lts-community
 
 
 # Installing AWS CLI
-#!/bin/bash
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 sudo apt install unzip -y
 unzip awscliv2.zip
 sudo ./aws/install
 
 # Installing Kubectl
-#!/bin/bash
 sudo apt update
 sudo apt install curl -y
 sudo curl -LO "https://dl.k8s.io/release/v1.28.4/bin/linux/amd64/kubectl"
@@ -50,20 +46,17 @@ kubectl version --client
 
 
 # Installing eksctl
-#! /bin/bash
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
 
 # Installing Terraform
-#!/bin/bash
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
 sudo apt install terraform -y
 
 # Installing Trivy
-#!/bin/bash
 sudo apt-get install wget apt-transport-https gnupg lsb-release -y
 wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo apt-key add -
 echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee -a /etc/apt/sources.list.d/trivy.list
@@ -72,5 +65,4 @@ sudo apt install trivy -y
 
 
 # Intalling Helm
-#! /bin/bash
 sudo snap install helm --classic
