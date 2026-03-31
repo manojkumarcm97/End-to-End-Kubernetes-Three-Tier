e767d13d324f1f91b5daa9f1859de8575d412c22
manoj kumar <manojkumarcm97@gmail.com>
2026-03-31 12:39:11 +0530

    modified terraform folder

--- terraform/gather.tf
+++ terraform/gather.tf
@@ -1,8 +1,8 @@
 data "aws_ami" "ami" {
-    most_recent = true
-    filter {
-        name   = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-    }
-    owners = ["271691289720"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  owners = ["271691289720"]
 }
